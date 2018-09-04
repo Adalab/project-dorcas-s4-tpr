9075f9713e8b73160cc5462aedb3e66af5639ed8
Olatz Aranzabe <olatzaranzabe@gmail.com>
2018-09-04 17:35:03 +0200

    install router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App';
 import PropTypes from 'prop-types';
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HashRouter> <App /> </HashRouter>, document.getElementById('react-root'));
 
