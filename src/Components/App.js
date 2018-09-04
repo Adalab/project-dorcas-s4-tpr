a8b92eac7842dd9dcf4ab30cf4e3e03ac9d3beb5
oljuanfe <ojuanferrero@gmail.com>
2018-09-04 18:49:58 +0200

    Create Footer component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Main from './Main.js';
+import Footer from './Footer.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Main />
+        <Footer/>
       </div>
     );
   }
---
+++ src/Components/Footer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import './Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Footer;
