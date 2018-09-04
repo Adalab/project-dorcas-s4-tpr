33b7b70f50fe7e6eff7b5a14236ef1fa1ac70dc8
Olatz Aranzabe <olatzaranzabe@gmail.com>
2018-09-04 17:22:16 +0200

    start components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Components/App';
 import PropTypes from 'prop-types';
 
 ReactDOM.render(<App />, document.getElementById('root'));
