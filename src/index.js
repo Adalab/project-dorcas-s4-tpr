98446df7ac12c26f9bb642ea58ed6a944696f227
Olatz Aranzabe <olatzaranzabe@gmail.com>
2018-09-04 17:17:42 +0200

    install proptypes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import PropTypes from 'prop-types';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
