1d10f6ae7d0b0cf8a00f0d5a7c010207d02068c4
ElisabetAS <elisabet240498@gmail.com>
2018-09-06 23:45:31 +0200

    finshed TypeTextInput

--- src/Components/Step2.js
+++ src/Components/Step2.js
@@ -6,44 +6,53 @@ import TypeTextInput from './TypeTextInput';
 
 const countryIssueInput = {
     labelContent: <FormattedMessage
-                   id="Step2.userPassCountryIssue"
-                   defaultMessage="Country of issue"
-                   />,
+        id="Step2.userPassCountryIssue"
+        defaultMessage="Country of issue"
+    />,
     id: 'countryIssue',
     name: 'countryIssue'
 };
 
 const birthPlaceInput = {
     labelContent: <FormattedMessage
-                   id="Step2.userBirthPlace"
-                   defaultMessage="Place of birth"
-                   />,
+        id="Step2.userBirthPlace"
+        defaultMessage="Place of birth"
+    />,
     id: 'birthPlace',
     name: 'birthPlace'
 };
 
 const numPassportInput = {
     labelContent: <FormattedMessage
-                   id="Step2.userPassportNumber"
-                   defaultMessage="Passport number"
-                   />,
+        id="Step2.userPassportNumber"
+        defaultMessage="Passport number"
+    />,
     id: 'numPassport',
     name: 'numPassport'
 };
 
+const documentNumberInput = {
+    labelContent: <FormattedMessage
+        id="Step2.userDocumentNumber"
+        defaultMessage="Document number"
+    />,
+    id: 'documentNumber',
+    name: 'documentNumber'
+};
+
 class Step2 extends Component {
-    render() { 
+    render() {
         const {
             title1,
             title2,
-            title3, 
+            title3,
             title4,
             title5,
             step2,
             dot1,
             dot2,
-            dot3, 
-            dot4, 
+            dot3,
+            dot4,
             dot5,
             previousStep,
             followingStep,
@@ -51,118 +60,115 @@ class Step2 extends Component {
             handleClickFollowingStep
         } = this.props;
         console.log('props STEP2', this.props);
-            return (
+        return (
             <div className='stepBox'>
-            <Title 
-                title={title2} 
-                step={step2}
-            />
-            <form className='form'>
+                <Title
+                    title={title2}
+                    step={step2}
+                />
+                <form className='form'>
 
-                        <h2>
+                    <h2>
+                        <FormattedMessage
+                            id="Step2.passport"
+                            defaultMessage="Passport"
+                        />
+                    </h2>
+
+                    <TypeTextInput inputText={numPassportInput} />
+                    <TypeTextInput inputText={countryIssueInput} />
+
+                    <div className='verticalDisplay'>
+                        <label htmlFor='dateIssue'>
                             <FormattedMessage
-                                id="Step2.passport"
-                                defaultMessage="Passport"
+                                id="Step2.userPassDateIssue"
+                                defaultMessage="Date of issue"
                             />
-                        </h2>
+                        </label>
+                        <input id='dateIssue' type='number' name='dateIssue' value='' />
 
-                        <TypeTextInput inputText={numPassportInput} />
-                        <TypeTextInput inputText={countryIssueInput} />
-                        <div className='verticalDisplay'>
-                            <label htmlFor='dateIssue'>
+                        <label htmlFor='expirationDate'>
                             <FormattedMessage
-                                    id="Step2.userPassDateIssue"
-                                    defaultMessage="Date of issue"
-                                />
-                            </label>
-                            <input id='dateIssue' type='number' name='dateIssue' value='' />
-                            <label htmlFor='expirationDate'>
-                            <FormattedMessage
-                                    id="Step2.userPassExpirationDate"
-                                    defaultMessage="Expiration date"
-                                />
-                            </label>
-                            <input id='expirationDate' type='number' name='expirationDate' value='' />
-                        </div>
-
-                        <h2>
-                            <FormattedMessage
-                                id="Step2.visa"
-                                defaultMessage="Visa (optional)"
+                                id="Step2.userPassExpirationDate"
+                                defaultMessage="Expiration date"
                             />
-                        </h2>
+                        </label>
+                        <input id='expirationDate' type='number' name='expirationDate' value='' />
+                    </div>
 
-                        <div>
-                            <FormattedMessage
-                                id="Step2.addVisaDetails"
-                                defaultMessage="Add visa details"
-                            />
-                        </div>
+                    <h2>
+                        <FormattedMessage
+                            id="Step2.visa"
+                            defaultMessage="Visa (optional)"
+                        />
+                    </h2>
 
-                        <h2>
-                            <FormattedMessage
-                                id="Step2.dniNie"
-                                defaultMessage="Id"
-                            />
-                        </h2>
+                    <div>
+                        <FormattedMessage
+                            id="Step2.addVisaDetails"
+                            defaultMessage="Add visa details"
+                        />
+                    </div>
 
-                        <div className='verticalDisplay'>
-                            <label htmlFor='documentType'>
-                            </label>
-                            <select id='documentType' name='documentType' value=''>
-                                <option value="">
-                                    <FormattedMessage
-                                        id="Step2.type"
-                                        defaultMessage="Type"
-                                    />
-                                </option>
+                    <h2>
+                        <FormattedMessage
+                            id="Step2.dniNie"
+                            defaultMessage="Id"
+                        />
+                    </h2>
 
-                            </select>
-
-                            <label htmlFor='documentNumber'>
+                    <div className='verticalDisplay'>
+                        <label htmlFor='documentType'>
+                        </label>
+                        <select id='documentType' name='documentType' value=''>
+                            <option value="">
                                 <FormattedMessage
-                                    id="Step2.userDocumentNumber"
-                                    defaultMessage="Document number"
+                                    id="Step2.type"
+                                    defaultMessage="Type"
                                 />
-                            </label>
+                            </option>
+                        </select>
 
-                            <input id='documentNumber' type='text' name='documentNumber' value='' />
-                        </div>
-                        <div className='verticalDisplay'>
-                            <label htmlFor='dateIssue'>
+                        <TypeTextInput inputText={documentNumberInput} />
+                    </div>
+
+                    <div className='verticalDisplay'>
+                        <label htmlFor='dateIssue'>
                             <FormattedMessage
-                                    id="Step2.userDocDateIssue"
-                                    defaultMessage="Date of issue"
-                                />
-                            </label>
-                            <input id='dateIssue' type='number' name='dateIssue' value='' />
-                            <label htmlFor='expirationDate'>
+                                id="Step2.userDocDateIssue"
+                                defaultMessage="Date of issue"
+                            />
+                        </label>
+                        <input id='dateIssue' type='number' name='dateIssue' value='' />
+
+                        <label htmlFor='expirationDate'>
                             <FormattedMessage
-                                    id="Step2.userDocExpirationDate"
-                                    defaultMessage="Expiration date"
-                                />
-                            </label>
-                            <input id='expirationDate' type='number' name='expirationDate' value='' />
-                        </div>
-                        <TypeTextInput inputText={birthPlaceInput} />
+                                id="Step2.userDocExpirationDate"
+                                defaultMessage="Expiration date"
+                            />
+                        </label>
+                        <input id='expirationDate' type='number' name='expirationDate' value='' />
+                    </div>
+
+                    <TypeTextInput inputText={birthPlaceInput} />
+                </form>
                 
-                    </form>
-            <Navigation 
-                title1={title1}
-                title2={title2}
-                title3={title3}
-                title4={title4}
-                title5={title5}
-                dot1={dot1} 
-                dot2={dot2}
-                dot3={dot3}
-                dot4={dot4}
-                dot5={dot5}
-                previousStep={previousStep} 
-                followingStep={followingStep}
-                handleClickPreviousStep={handleClickPreviousStep}
-                handleClickFollowingStep={handleClickFollowingStep}
-            />
+                <Navigation
+                    title1={title1}
+                    title2={title2}
+                    title3={title3}
+                    title4={title4}
+                    title5={title5}
+                    dot1={dot1}
+                    dot2={dot2}
+                    dot3={dot3}
+                    dot4={dot4}
+                    dot5={dot5}
+                    previousStep={previousStep}
+                    followingStep={followingStep}
+                    handleClickPreviousStep={handleClickPreviousStep}
+                    handleClickFollowingStep={handleClickFollowingStep}
+                />
             </div>
         );
     }
