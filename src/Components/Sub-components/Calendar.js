353256a0a00956462faad07ff8a3fffa04538803
ElisabetAS <elisabet240498@gmail.com>
2018-09-10 19:04:05 +0200

    add size inputs calendar

--- src/Components/Sub-components/Calendar.js
+++ src/Components/Sub-components/Calendar.js
@@ -1,34 +1,37 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { SingleDatePicker } from 'react-dates';
 
 class Calendar extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-                date: "",
-                focused: false,
+            date: "",
+            focused: false,
         }
     }
     render() {
         console.log(this.props);
-        const { id, labelContent, numberOfMonths, small} = this.props;
+        const { id, labelContent, numberOfMonths, small } = this.props;
         return (
-            <Fragment>
-            <label htmlFor='dateIssue'>
-               {labelContent} 
-            </label>
-            <SingleDatePicker
-                date={this.state.date} // momentPropTypes.momentObj or null
-                onDateChange={date => this.setState({ date })} // PropTypes.func.isRequired
-                focused={this.state.focused} // PropTypes.bool
-                onFocusChange={({ focused }) => this.setState({ focused })} // PropTypes.func.isRequired
-                id={id} // PropTypes.string.isRequired
-                numberOfMonths={numberOfMonths}
-                small={small}
-                autocomplete="on"
-        />
-        </Fragment>
-         );
+            <div className='position-calendar'>
+                <SingleDatePicker
+                    date={this.state.date} // momentPropTypes.momentObj or null
+                    onDateChange={date => this.setState({ date })} // PropTypes.func.isRequired
+                    focused={this.state.focused} // PropTypes.bool
+                    onFocusChange={({ focused }) => this.setState({ focused })} // PropTypes.func.isRequired
+                    id={id} // PropTypes.string.isRequired
+                    numberOfMonths={numberOfMonths}
+                    small={small}
+                    autocomplete="on"
+                />
+                
+                <label 
+                className='placeholder-label'
+                htmlFor='dateIssue'>
+                {labelContent}
+                </label>
+            </div>
+        );
     }
 }
 
