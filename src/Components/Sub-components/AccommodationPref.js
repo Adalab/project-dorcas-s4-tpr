61b2fab4538e94134e881250dc7c2b1851eab508
Olatz Aranzabe <olatzaranzabe@gmail.com>
2018-09-12 16:22:33 +0200

    delete a fragment

--- src/Components/Sub-components/AccommodationPref.js
+++ src/Components/Sub-components/AccommodationPref.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 
 class AccommodationPref extends Component {
