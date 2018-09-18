ad64d1f906404f050392c4d280d820702e7e3cf2
Sinsit1 <alexandra8989@gmail.com>
2018-09-19 00:04:31 +0200

    wip

--- src/Components/Base.js
+++ src/Components/Base.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import App from './App';
 import {Route, Switch, Redirect} from 'react-router';
+import ErrorPage from './ErrorPage';
 
 class Base extends Component {
     render() { 
@@ -9,16 +10,23 @@ class Base extends Component {
             <Route
             exact
             path='/'
-            render={props => (<Redirect to={`/step/1/${props.match.params.id}`}/>)}
+            render={props => (<Redirect to={`/step/
+            ${props.match.params.id===undefined?'404':'1'}`}/>)}
             />
             <Route 
             path='/step/:stepNumber/:id'
             render={props =>
-            <App 
-                id={props.match.params.id}
-            /> 
-        }  
-    />
+                <App 
+                    id={props.match.params.id}
+                /> 
+                }         
+            />
+            <Route 
+                exact path='/step/404'
+                    render={props => 
+                        <ErrorPage />
+                }
+             />       
     </Switch>);
     }
 }
